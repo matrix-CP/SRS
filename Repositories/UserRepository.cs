7e2a5881ec1f02d8c1d689b34e6570f4155ad7c7
yugjariwala <yugjari1708@gmail.com>
2024-02-22 15:43:24 +0530

    Repository

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SRS.Repositories
+{
+    public class UserRepository
+    {
+        
+    }
+}
